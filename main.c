e2d00103194a816bcbc1b816f0b56c46f7bfd216
Mubarek Seid <mubareksd@gmail.com>
2022-08-10 12:44:06 +0300

    init

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: number of arguments received
+ * @argv: arguments received
+ * Return: 0
+**/
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
